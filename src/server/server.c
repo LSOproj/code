d8e315ca1d94966a908311a2b5c99fd63b5fa949
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2026-01-26 14:16:13 +0100

    init

---
+++ src/server/server.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(){
+	
+	int server_socket;
+	struct sockarrd_un server_address;
+
+
+
+	return 0;
+}
