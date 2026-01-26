a0de7b1846edf093ce142629409777b0fbeed8f3
LucaPiccolo7 <luca.piccolo0705@gmail.com>
2026-01-26 23:00:49 +0100

    Integrato sqlite3
    - Fatte funzioni per create table

--- src/server/server.c
+++ src/server/server.c
@@ -8,15 +8,63 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <sqlite3.h>
+#include <time.h>
 
-#define MAX_ACCEPTED_CLIENT 50
+#define MAX_ACCEPTED_CLIENT 	50
+#define MAX_USER_USERNAME_SIZE	100
+#define MAX_USER_PASSWORD_SIZE	100
+#define MAX_FILM_TITLE_SIZE	100
+
+typedef struct user {
+
+	int id;
+	char username[MAX_USER_USERNAME_SIZE];
+	char password[MAX_USER_PASSWORD_SIZE];
+
+} user;
+
+typedef struct film {
+
+	int id;
+	char title[MAX_FILM_TITLE_SIZE];
+	int available_copies;
+	int rented_out_copies;
+
+} film;
+
+typedef struct reservation {
+
+	int id;
+	time_t rental_date;
+	time_t due_date;
+	int user_id;
+	int film_id;
+
+} reservation; 
 
 void error_handler(char *message);
+void database_connection_init(sqlite3** database);
+void database_user_table_init(sqlite3* database);
+void database_film_table_init(sqlite3* database);
+void database_reservation_table_init(sqlite3* database);
 void* connection_handler(void* arg);
 
 int main(){
 	
 	int server_socket, client_socket;
+	sqlite3 *database;
+
+	database_connection_init(&database);
+
+	printf("\n[SERVER] Database connesso.\n");
+	
+	database_user_table_init(database);
+	database_film_table_init(database);
+	database_reservation_table_init(database);
+	
+	printf("\n[SERVER] Tabelle del database create.\n");
+
 	struct sockaddr_un server_address;
 	socklen_t server_address_len = sizeof(server_address);
 
@@ -61,6 +109,7 @@ int main(){
 	}
 
 	close(server_socket);
+	sqlite3_close(database);
 	unlink(server_address.sun_path);
 
 	return 0;
@@ -85,6 +134,58 @@ void* connection_handler(void* client_socket_arg){
 	close(client_socket);
 
 	pthread_exit(NULL);
+}
+
+void database_connection_init(sqlite3 **database){
+
+	if(sqlite3_open("database.db", database) != 0){
+		sqlite3_close(*database);
+		error_handler("[SERVER] Errore creazione/apertura database");
+	}
+}
+
+void database_user_table_init(sqlite3* database){
+
+	char *statement_sql =
+		"CREATE TABLE IF NOT EXISTS USER("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"username VARCHAR NOT NULL UNIQUE, "
+		"password VARCHAR NOT NULL"
+		");";
+		
+	if(sqlite3_exec(database, statement_sql, 0, 0, NULL) != 0)
+		error_handler("[SERVER] Errore creazione USER table sqlite");
+}
+
+void database_film_table_init(sqlite3* database){
+
+	char *statement_sql =
+		"CREATE TABLE IF NOT EXISTS FILM("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"title VARCHAR NOT NULL, "
+		"available_copies INTEGER NOT NULL, "
+		"rented_out_copies INTEGER NOT NULL DEFAULT 0"
+		");";
+		
+	if(sqlite3_exec(database, statement_sql, 0, 0, NULL) != 0)
+		error_handler("[SERVER] Errore creazione FILM table sqlite");
+}
+
+void database_reservation_table_init(sqlite3* database){
+
+	char *statement_sql =
+		"CREATE TABLE IF NOT EXISTS RESERVATION("
+		"id INTEGER PRIMARY KEY AUTOINCREMENT, "
+		"rental_date INTEGER NOT NULL, "
+		"due_date INTEGER, "
+		"user_id INTEGER NOT NULL, "
+		"film_id INTEGER NOT NULL, "
+		"FOREIGN KEY (user_id) REFERENCES USER(id), "
+        	"FOREIGN KEY (film_id) REFERENCES FILM(id)"
+        	");";
+		
+	if(sqlite3_exec(database, statement_sql, 0, 0, NULL) != 0)
+		error_handler("[SERVER] Errore creazione RESERVATION table sqlite");
 }
 
 void error_handler(char *message){
